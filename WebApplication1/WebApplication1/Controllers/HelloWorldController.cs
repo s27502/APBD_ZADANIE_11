0ef8e9d885bf1520b708b9848e385ff74cc96cd0
Jakub Rychta <s27502@pjwstk.edu.pl>
2024-06-21 09:39:11 +0200

    a

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+public class HelloWorldController : Controller
+{
+    
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
